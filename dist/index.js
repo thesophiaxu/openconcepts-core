a345a4f5c5baf1afcf9ecd12ba741fc91a26454f
Haoji Xu <haoji.xu@outlook.com>
2019-11-20 15:16:19 +0000

    Refactor Working in Progress

--- dist/index.js
+++ dist/index.js
@@ -9,35 +9,47 @@ var mongodb_1 = __importDefault(require("mongodb"));
 var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
 var wiki = "wikidatawiki";
 var dbUrl = 'mongodb://localhost:27017';
-var dbName = 'openconcepts';
-var collectionName = "wikidata_raw";
-var currentUpdates = 0;
-var dbConnection = new mongodb_1.default.MongoClient(dbUrl);
-dbConnection.connect(function (err) {
-    console.log("Connected successfully to server");
-    postDbInit();
-});
-function exportStats() {
-    console.timeEnd('timer');
-    console.log('processed: ' + currentUpdates);
-    currentUpdates = 0;
-    console.time('timer');
-}
-function postDbInit() {
-    console.time('timer');
-    console.log("Connecting to EventStreams at " + url);
-    var eventSource = new eventsource_1.default(url);
-    eventSource.onopen = function (event) {
-        console.log('--- Opened connection.');
+var dbName = 'openConceptsInfra';
+var collectionName = "wikidataRaw";
+var collectionNameJobs = "wikidataRawJobs";
+/**
+ * StreamToqrikoq: Listens a Apache KAFKA event stream and distributes site updates to a job queue defined by a database location.
+ * @etymology toqrikoq (t'oqrikoq): local leaders of the Mita system, typically manage a city and its hinterland.
+ */
+var StreamToqrikoq = /** @class */ (function () {
+    function StreamToqrikoq(args) {
+        this.url = "";
+        this.dateOffsetStr = "";
+        this.url = args.listeningURL;
+        this.dateOffsetStr = args.dateOffsetStr;
+        this.eventSource = new eventsource_1.default(this.url + this.dateOffsetStr);
+        console.log("[DEBUG] Connecting to EventStreams at " + this.eventSource.url);
+        this.jobQueueInterface = args.dbConnection.db(args._dbn).collection(args._colln);
+        this.registerMessageListeners();
+    }
+    StreamToqrikoq.prototype.registerMessageListeners = function () {
+        var _this = this;
+        this.eventSource.onmessage = function (evt) { return _this.onMessage.call(_this, evt); };
+        this.eventSource.onopen = function (evt) { return _this.onOpen.call(_this, evt); };
+        this.eventSource.onerror = function (evt) { return _this.onError.call(_this, evt); };
     };
-    eventSource.onerror = function (event) {
-        console.error('--- Encountered error', event);
+    StreamToqrikoq.prototype.onError = function (event) {
+        throw new Error("[DEBUG] An error occured.");
     };
-    eventSource.onmessage = function (event) {
+    StreamToqrikoq.prototype.onOpen = function (event) {
+        console.log('[DEBUG] Successfully connected to EventStream.');
+    };
+    /**
+     * onMessage: receives a message and pass it to jobs database.
+     * @param event The Apache KAFKA event.
+     */
+    StreamToqrikoq.prototype.onMessage = function (event) {
+        if (!event)
+            return; // Don't waste time here
         // event.data will be a JSON string containing the message event.
         var change = JSON.parse(event.data);
         //if (change.wiki == wiki) console.log("Got update "+change.title);
-        if (change.wiki == wiki && (change.title.charAt(0) == 'L' || change.title.charAt(0) == 'Q' || change.title.charAt(0) == 'P')) {
+        if (change.wiki == "wikidatawiki" && (change.title.charAt(0) == 'L' || change.title.charAt(0) == 'Q' || change.title.charAt(0) == 'P')) {
             //console.log(change);
             var finalTitle = change.title;
             // Pre-process title string
@@ -45,39 +57,122 @@ function postDbInit() {
                 finalTitle = change.title.substring(7);
             if (change.title.charAt(0) == 'P')
                 finalTitle = change.title.substring(9);
-            try {
-                request_1.default('https://www.wikidata.org/wiki/Special:EntityData/' + finalTitle + '.json', { json: true }, function (err, res, body) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    //console.log(Object.values(body.entities)[0]);
-                    var collection = dbConnection.db(dbName).collection(collectionName);
-                    // @ts-ignore: type "unknown" is actually JSON object
-                    //console.log("Update started "+finalTitle);
-                    if (body.entities != null && body.entities != undefined) {
-                        collection.findOneAndUpdate({ id: finalTitle }, { $set: Object.values(body.entities)[0] }, { upsert: true, returnOriginal: false }, function (err, doc) {
-                            if (err) {
-                                console.log("An error occured: id = " + finalTitle);
-                            }
-                            //else { console.log("Updated "+finalTitle+", _id: "+doc.value._id); }
-                            currentUpdates++;
-                            if (currentUpdates >= 50)
-                                exportStats();
-                        });
-                    }
-                    else {
-                        console.log("An error occured (body.entities = null): id = " + finalTitle);
-                        var waitTill = new Date(new Date().getTime() + 2 * 1000);
-                        while (waitTill > new Date()) {
-                            console.log("Cooling down!", (waitTill.getTime() - new Date().getTime()));
-                        }
-                    }
-                });
-            }
-            catch (e) {
-                console.log("An error occured: id = " + finalTitle);
-            }
+            this.jobQueueInterface.insertOne({
+                pageTitle: finalTitle,
+                pageURL: 'https://www.wikidata.org/wiki/Special:EntityData/' + finalTitle + '.json',
+                createdAt: Date.now()
+            }, function (err, r) {
+                if (err)
+                    throw new Error("[ERROR] An error occured.");
+            });
         }
         //console.log(JSON.parse(event.data));
     };
+    return StreamToqrikoq;
+}());
+var Worker = /** @class */ (function () {
+    function Worker(args) {
+        console.log("[DEBUG] Worker started!");
+        this.jobQueueInterface = args.dbConnection.db(args._dbn).collection(args._collnQueue);
+        this.dataInterface = args.dbConnection.db(args._dbn).collection(args._collnData);
+        this.ScheduleWork();
+    }
+    Worker.prototype.ScheduleWork = function () {
+        var _this = this;
+        setInterval(function () {
+            _this.jobQueueInterface.find().sort({ createdAt: 1 }).limit(1).toArray(function (err, doc) {
+                if (err)
+                    throw new Error("[ERROR] Worker: An error occured: " + err.message);
+                if (doc[0]) {
+                    _this.work(doc[0]._id);
+                }
+            });
+        }, 200); // every 0.2 second
+    };
+    Worker.prototype.work = function (workID) {
+        var myDataInterface = this.dataInterface;
+        var myJobQueueInterface = this.jobQueueInterface;
+        this.jobQueueInterface.findOne({ _id: workID }, function (err, document) {
+            if (err) {
+                throw new Error("[ERROR] something happened: " + err.message);
+            }
+            if (document) {
+                try {
+                    request_1.default(document.pageURL, { json: true }, function (err, res, body) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        //console.log(Object.values(body.entities)[0]);
+                        // @ts-ignore: type "unknown" is actually JSON object
+                        //console.log("Update started "+finalTitle);
+                        if (body.entities != null && body.entities != undefined) {
+                            myDataInterface.findOneAndUpdate({ id: document.pageTitle }, { $set: Object.values(body.entities)[0] }, { upsert: true, returnOriginal: false }, function (err, doc) {
+                                if (err) {
+                                    console.log("An error occured: id = " + document.pageTitle);
+                                }
+                                //else { console.log("Updated "+finalTitle+", _id: "+doc.value._id); }
+                                if (doc) {
+                                    myJobQueueInterface.deleteOne({ _id: workID });
+                                    console.log("[WORKER] My work here (" + workID + ") is done.");
+                                }
+                            });
+                        }
+                        else {
+                            console.log("An error occured (body.entities = null): id = " + document.pageTitle);
+                        }
+                    });
+                }
+                catch (e) {
+                    console.log("An error occured: id = " + document.pageTitle);
+                }
+            }
+        });
+    };
+    return Worker;
+}());
+var dbConnection = new mongodb_1.default.MongoClient(dbUrl);
+dbConnection.connect(function (err) {
+    console.log("Connected successfully to server");
+    postDbInit();
+});
+function postDbInit() {
+    var toq = new StreamToqrikoq({
+        listeningURL: url,
+        dateOffsetStr: "",
+        _dbn: dbName,
+        _colln: collectionNameJobs,
+        dbConnection: dbConnection
+    });
+    var worker = new Worker({
+        _dbn: dbName,
+        _collnQueue: collectionNameJobs,
+        _collnData: collectionName,
+        dbConnection: dbConnection
+    });
 }
+/*
+function postDbInit () {
+
+    console.time('timer')
+
+    console.log(`Connecting to EventStreams at ${url}`);
+    var eventSource = new EventSource(url);
+
+    eventSource.onopen = function(event) {
+        console.log('--- Opened connection.');
+    };
+
+    eventSource.onerror = function(event) {
+        console.error('--- Encountered error', event);
+    };
+
+    eventSource.onmessage = function(event) {
+        
+    };
+
+}*/
+/**
+ * Refactor notes:
+ * @before a function that is pretty much procedural that takes any event and insert it into the wikidata database. Very much not consistent and single-threaded.
+ * @after listeners and workers that work on the openConceptsInfra.wikidataRawJobs -
+ */ 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,143 @@
 import EventSource from 'eventsource';
 import request from 'request';
-import MongoClient from 'mongodb';
+import MongoClient, { Timestamp, ObjectID } from 'mongodb';
 
 var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
 var wiki = "wikidatawiki"
 
 var dbUrl = 'mongodb://localhost:27017';
-var dbName = 'openconcepts';
-var collectionName = "wikidata_raw";
+var dbName = 'openConceptsInfra';
+var collectionName = "wikidataRaw";
 
-var currentUpdates = 0;
+var collectionNameJobs: string = "wikidataRawJobs";
+
+interface JobSchema {
+    pageTitle: string,
+    pageURL: string,
+    createdAt: Timestamp
+}
+
+/**
+ * StreamToqrikoq: Listens a Apache KAFKA event stream and distributes site updates to a job queue defined by a database location.
+ * @etymology toqrikoq (t'oqrikoq): local leaders of the Mita system, typically manage a city and its hinterland.
+ */
+class StreamToqrikoq {
+    url: string = "";
+    dateOffsetStr: string = "";
+
+    eventSource: EventSource;
+    jobQueueInterface: MongoClient.Collection;
+
+    constructor (args: {listeningURL: string, dateOffsetStr: string, _dbn: string, _colln: string, dbConnection: MongoClient.MongoClient}) {
+        this.url = args.listeningURL;
+        this.dateOffsetStr = args.dateOffsetStr;
+
+        this.eventSource = new EventSource(this.url + this.dateOffsetStr);
+        console.log(`[DEBUG] Connecting to EventStreams at ${this.eventSource.url}`);
+
+        this.jobQueueInterface = args.dbConnection.db(args._dbn).collection(args._colln);
+
+        this.registerMessageListeners();
+    }
+
+    registerMessageListeners () {
+        this.eventSource.onmessage = (evt) => this.onMessage.call(this, evt);
+        this.eventSource.onopen = (evt) => this.onOpen.call(this, evt);
+        this.eventSource.onerror = (evt) => this.onError.call(this, evt);
+    }
+
+    onError(event: MessageEvent | undefined): (evt: MessageEvent) => any {
+        throw new Error("[DEBUG] An error occured.");
+    }
+
+    onOpen(event: MessageEvent | undefined): void {
+        console.log ('[DEBUG] Successfully connected to EventStream.')
+    }
+    
+    /**
+     * onMessage: receives a message and pass it to jobs database.
+     * @param event The Apache KAFKA event.
+     */
+    onMessage(event: MessageEvent | undefined): void {
+        if (!event) return; // Don't waste time here
+        // event.data will be a JSON string containing the message event.
+        var change = JSON.parse(event.data);
+        //if (change.wiki == wiki) console.log("Got update "+change.title);
+        if (change.wiki == "wikidatawiki" && (change.title.charAt(0) == 'L' || change.title.charAt(0) == 'Q' || change.title.charAt(0) == 'P')) {
+            //console.log(change);
+            
+            var finalTitle = change.title;
+
+            // Pre-process title string
+            if (change.title.charAt(0) == 'L') finalTitle = change.title.substring(7);
+            if (change.title.charAt(0) == 'P') finalTitle = change.title.substring(9);
+
+            this.jobQueueInterface.insertOne ({
+                pageTitle: finalTitle,
+                pageURL: 'https://www.wikidata.org/wiki/Special:EntityData/'+finalTitle+'.json',
+                createdAt: Date.now()
+            }, function(err, r) {
+                if (err) throw new Error("[ERROR] An error occured.");
+            });
+        }
+        //console.log(JSON.parse(event.data));
+    }
+}
+
+class Worker {
+    jobQueueInterface: MongoClient.Collection;
+    dataInterface: MongoClient.Collection;
+
+    constructor (args: {_dbn: string, _collnQueue: string, _collnData: string, dbConnection: MongoClient.MongoClient}) {
+        console.log(`[DEBUG] Worker started!`);
+
+        this.jobQueueInterface = args.dbConnection.db(args._dbn).collection(args._collnQueue);
+        this.dataInterface = args.dbConnection.db(args._dbn).collection(args._collnData);
+
+        this.ScheduleWork();
+    }
+
+    ScheduleWork(): void {
+        setInterval(() => {
+            this.jobQueueInterface.find().sort( {createdAt: 1 }).limit(1).toArray((err, doc) => {
+                if (err) throw new Error("[ERROR] Worker: An error occured: "+ err.message);
+                if (doc[0]) {this.work(doc[0]._id)}
+            });
+        }, 200) // every 0.2 second
+    }
+
+    work (workID: ObjectID): void {
+        var myDataInterface = this.dataInterface;
+        var myJobQueueInterface = this.jobQueueInterface;
+        this.jobQueueInterface.findOne({_id: workID}, function (err, document) {
+            if (err) { throw new Error("[ERROR] something happened: " + err.message);}
+            if (document) {
+                try {
+                    request(document.pageURL, { json: true }, (err, res, body) => {
+                        if (err) { return console.log(err); }
+                        //console.log(Object.values(body.entities)[0]);
+                        // @ts-ignore: type "unknown" is actually JSON object
+                        //console.log("Update started "+finalTitle);
+                        if (body.entities != null && body.entities != undefined) {
+                            myDataInterface.findOneAndUpdate(
+                                {id: document.pageTitle}, 
+                                {$set: Object.values(body.entities)[0]}, 
+                                {upsert: true, returnOriginal: false}, 
+                                function(err,doc) {
+                                    if (err) { console.log ("An error occured: id = "+ document.pageTitle) }
+                                    //else { console.log("Updated "+finalTitle+", _id: "+doc.value._id); }
+                                    if (doc) {myJobQueueInterface.deleteOne({_id: workID}); console.log(`[WORKER] My work here (${workID}) is done.`)}
+                                });  
+                        } else {console.log ("An error occured (body.entities = null): id = "+document.pageTitle);}
+                    });
+                } catch (e) {
+                    console.log ("An error occured: id = "+document.pageTitle)
+                } 
+            }
+        })
+    }
+
+}
 
 var dbConnection = new MongoClient.MongoClient(dbUrl);
 
@@ -20,13 +148,25 @@ dbConnection.connect(function(err) {
 
 });
 
-function exportStats () {
-    console.timeEnd('timer')
-    console.log('processed: ' + currentUpdates)
-    currentUpdates = 0;
-    console.time('timer')
+function postDbInit () {
+    var toq: StreamToqrikoq = new StreamToqrikoq({
+        listeningURL: url,
+        dateOffsetStr: "",
+        _dbn: dbName,
+        _colln: collectionNameJobs,
+        dbConnection: dbConnection
+    })
+
+    var worker: Worker = new Worker({
+        _dbn: dbName,
+        _collnQueue: collectionNameJobs,
+        _collnData: collectionName,
+        dbConnection: dbConnection
+    })
+
 }
 
+/*
 function postDbInit () {
 
     console.time('timer')
@@ -43,40 +183,13 @@ function postDbInit () {
     };
 
     eventSource.onmessage = function(event) {
-        // event.data will be a JSON string containing the message event.
-        var change = JSON.parse(event.data);
-        //if (change.wiki == wiki) console.log("Got update "+change.title);
-        if (change.wiki == wiki && (change.title.charAt(0) == 'L' || change.title.charAt(0) == 'Q' || change.title.charAt(0) == 'P')) {
-            //console.log(change);
-            
-            var finalTitle = change.title;
-
-            // Pre-process title string
-            if (change.title.charAt(0) == 'L') finalTitle = change.title.substring(7);
-            if (change.title.charAt(0) == 'P') finalTitle = change.title.substring(9);
-            try {
-                request('https://www.wikidata.org/wiki/Special:EntityData/'+finalTitle+'.json', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    //console.log(Object.values(body.entities)[0]);
-                    const collection = dbConnection.db(dbName).collection(collectionName)
-                    // @ts-ignore: type "unknown" is actually JSON object
-                    //console.log("Update started "+finalTitle);
-                    if (body.entities != null && body.entities != undefined) {
-                        collection.findOneAndUpdate({id: finalTitle}, {$set: Object.values(body.entities)[0]}, {upsert: true, returnOriginal: false}, function(err,doc) {
-                            if (err) { console.log ("An error occured: id = "+finalTitle) }
-                            //else { console.log("Updated "+finalTitle+", _id: "+doc.value._id); }
-                            currentUpdates ++;
-                            if (currentUpdates >= 50) exportStats ();
-                        });  
-                    } else {console.log ("An error occured (body.entities = null): id = "+finalTitle); var waitTill = new Date(new Date().getTime() + 2 * 1000);
-                    while(waitTill > new Date()){console.log("Cooling down!", (waitTill.getTime() - new Date().getTime()))}}
-                });
-            } catch (e) {
-                console.log ("An error occured: id = "+finalTitle)
-            }
-        }
-        //console.log(JSON.parse(event.data));
+        
     };
 
-}
+}*/
 
+/**
+ * Refactor notes:
+ * @before a function that is pretty much procedural that takes any event and insert it into the wikidata database. Very much not consistent and single-threaded.
+ * @after listeners and workers that work on the openConceptsInfra.wikidataRawJobs - 
+ */
